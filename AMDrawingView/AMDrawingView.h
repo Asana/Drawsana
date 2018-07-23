c14af378f37e262969324f61a3ac4aa351ded607
Steve Landey <stevelandey@asana.com>
2018-07-23 10:37:39 -0700

    Initial Commit

---
+++ AMDrawingView/AMDrawingView.h
@@ -0,0 +1,19 @@
+//
+//  AMDrawingView.h
+//  AMDrawingView
+//
+//  Created by Steve Landey on 7/23/18.
+//  Copyright © 2018 Asana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AMDrawingView.
+FOUNDATION_EXPORT double AMDrawingViewVersionNumber;
+
+//! Project version string for AMDrawingView.
+FOUNDATION_EXPORT const unsigned char AMDrawingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMDrawingView/PublicHeader.h>
+
+
