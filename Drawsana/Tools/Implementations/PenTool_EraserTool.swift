a7c679c0b7da6c10a32909ba175e68e9d82d3451
Steve Landey <stevelandey@asana.com>
2018-08-10 18:43:48 -0700

    Pen tool renders everything to a buffer image in order to support transparency

--- Drawsana/Tools/Implementations/PenTool_EraserTool.swift
+++ Drawsana/Tools/Implementations/PenTool_EraserTool.swift
@@ -12,14 +12,17 @@ public class PenTool: DrawingTool {
   public typealias ShapeType = PenShape
 
   public var name: String { return "Pen" }
-
   public var shapeInProgress: PenShape?
-
-  public var isProgressive: Bool { return true }
+  public var isProgressive: Bool { return false }
+  public var velocityBasedWidth: Bool = true
 
   private var lastVelocity: CGPoint = .zero
-
-  public var velocityBasedWidth: Bool = true
+  // The shape is rendered to a buffer so that if the color is transparent,
+  // you see one contiguous line instead of a bunch of overlapping line
+  // segments.
+  private var shapeInProgressBuffer: UIImage?
+  private var drawingSize: CGSize = .zero
+  private var alpha: CGFloat = 0
 
   public init() { }
 
@@ -32,10 +35,15 @@ public class PenTool: DrawingTool {
   }
 
   public func handleDragStart(context: ToolOperationContext, point: CGPoint) {
+    drawingSize = context.drawing.size
+    var white: CGFloat = 0  // ignored
+    context.userSettings.strokeColor?.getWhite(&white, alpha: &self.alpha)
     lastVelocity = .zero
-    shapeInProgress = PenShape()
-    shapeInProgress?.start = point
-    shapeInProgress?.apply(userSettings: context.userSettings)
+    let shape = PenShape()
+    shapeInProgress = shape
+    shape.start = point
+    shape.apply(userSettings: context.userSettings)
+    shape.strokeColor = shape.strokeColor.withAlphaComponent(1)
   }
 
   public func handleDragContinue(context: ToolOperationContext, point: CGPoint, velocity: CGPoint) {
@@ -59,16 +67,23 @@ public class PenTool: DrawingTool {
   public func handleDragEnd(context: ToolOperationContext, point: CGPoint) {
     guard let shapeInProgress = shapeInProgress else { return }
     shapeInProgress.isFinished = true
+    shapeInProgress.apply(userSettings: context.userSettings)
     context.operationStack.apply(operation: AddShapeOperation(shape: shapeInProgress))
     self.shapeInProgress = nil
+    shapeInProgressBuffer = nil
   }
 
   public func handleDragCancel(context: ToolOperationContext, point: CGPoint) {
     shapeInProgress = nil
+    shapeInProgressBuffer = nil
   }
 
   public func renderShapeInProgress(transientContext: CGContext) {
-    shapeInProgress?.renderLatestSegment(in: transientContext)
+    shapeInProgressBuffer = DrawsanaUtilities.renderImage(size: drawingSize) {
+      self.shapeInProgressBuffer?.draw(at: .zero)
+      self.shapeInProgress?.renderLatestSegment(in: $0)
+    }
+    shapeInProgressBuffer?.draw(at: .zero, blendMode: .overlay, alpha: alpha)
   }
 }
 
