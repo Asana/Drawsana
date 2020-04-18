b6766ba07cc7687ea44e3af49574d479a8dfc801
ZY <zzymoon@gmail.com>
2020-04-18 19:56:08 +0800

    Fix the bug that can't input Chinese character

--- Drawsana/Tools/Implementations/Text tool/TextTool.swift
+++ Drawsana/Tools/Implementations/Text tool/TextTool.swift
@@ -238,7 +238,7 @@ public class TextTool: NSObject, DrawingTool {
 
   func updateTextView() {
     guard let shape = selectedShape else { return }
-    editingView.textView.text = shape.text
+    // editingView.textView.text = shape.text
     editingView.textView.font = shape.font
     editingView.textView.textColor = shape.fillColor
     editingView.bounds = shape.boundingRect
